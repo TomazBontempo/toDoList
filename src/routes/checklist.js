b8f9d607a80237d4eedd7fff112689bca941d5fd
Tomaz Bontempo <tomaz.bontempo@gmail.com>
2023-02-17 21:44:46 -0300

    Finishing the exercise

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const checklist = require("../models/checklist");
 const router = express.Router();
 const Checklist = require("../models/checklist");
 
@@ -6,52 +7,76 @@ router.get("/", async (req, res) => {
   try {
     let checklists = await Checklist.find({});
     res.status(200).render("checklists/index", { checklists: checklists });
-  } catch {
+  } catch (error) {
     res.status(500).render("pages/error", { error: "Fail to show lists" });
   }
 });
 
-router.post("/", async (req, res) => {
-  let { name } = req.body;
+router.get("/new", async (req, res) => {
   try {
-    let checklist = await Checklist.create({ name });
-    res.status(200).json(checklist);
-  } catch {
-    res.status(422).json(error);
+    let checklist = new Checklist();
+    res.status(200).render("checklists/new", { checklist: checklist });
+  } catch (error) {
+    res.status(500).render("pages/error", { error: "Fail to load form" });
+  }
+});
+
+router.get("/:id/edit", async (req, res) => {
+  try {
+    let checklist = await Checklist.findById(req.params.id);
+    res.status(200).render("checklists/edit", { checklist: checklist });
+  } catch (error) {
+    res.status(500).render("pages/error", { error: "Fail to edit page" });
+  }
+});
+
+router.post("/", async (req, res) => {
+  let { name } = req.body.checklist;
+  let checklist = new Checklist({ name });
+  try {
+    await checklist.save();
+    res.redirect("/checklists");
+  } catch (error) {
+    res
+      .status(422)
+      .render("checklists/new", { checklists: { ...checklist, error } });
   }
 });
 
 router.get("/:id", async (req, res) => {
   try {
-    let checklist = await Checklist.findById(req.params.id);
+    let checklist = await Checklist.findById(req.params.id).populate("tasks");
     res.status(200).render("checklists/show", { checklist: checklist });
-  } catch {
+  } catch (error) {
     res
-      .status(422)
-      .render("pages/error", { error: "Fail to show tasks lists" });
+      .status(500)
+      .render("pages/error", { error: "Error on showing the checklist" });
   }
 });
 
 router.put("/:id", async (req, res) => {
+  let { name } = req.body.checklist;
+  let checklist = await Checklist.findById(req.params.id);
+
   try {
-    let { name } = req.body;
-    let checklist = await Checklist.findByIdAndUpdate(
-      req.params.id,
-      { name },
-      { new: true }
-    );
-    res.status(200).json(checklist);
-  } catch {
-    res.status(422).json(error);
+    await checklist.update({ name });
+    res.redirect("/checklists");
+  } catch (error) {
+    let errors = error.errors;
+    res
+      .status(422)
+      .render("checklists/edit", { checklist: { ...checklist, errors } });
   }
 });
 
 router.delete("/:id", async (req, res) => {
   try {
     let checklist = await Checklist.findByIdAndRemove(req.params.id);
-    res.status(200).json(checklist);
-  } catch {
-    res.status(422).json(error);
+    res.redirect("/checklists");
+  } catch (error) {
+    res
+      .status(500)
+      .render("pages/error", { error: "Error on deleting the checklist" });
   }
 });
 
